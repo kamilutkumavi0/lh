9f9453cb9d49f2ffc7a56fb5d65c2e26dc8347d3
Kamil Utku Mavi <kamilutkumavi0@gmail.com>
2024-01-23 23:03:12 +0300

    tabular print done

--- src/output_printer.rs
+++ src/output_printer.rs
@@ -1,28 +1,29 @@
 //! Prints output with handsomely
+mod formatter;
+use formatter::{OutputElement, Output};
 use crate::parserer::Args;
 use crate::file_reader::Element;
-use colored::Colorize;
+use colored::{Colorize, ColoredString};
 use crate::tomlread::ColorFormat;
+use terminal_size::{Width, Height, terminal_size};
+
 pub fn output_print(_parsed_args: &Args, 
 					filtered_files: Vec<Element>){
-	for i in filtered_files{
-		match i.file_type.clone().unwrap().color{
-			ColorFormat::Black => {print!("{} {} ",i.file_type.unwrap().symbol.black(), i.name.black());},
-			ColorFormat::Red => {print!("{} {} ",i.file_type.unwrap().symbol.red(), i.name.red());},
-			ColorFormat::Green => {print!("{} {} ",i.file_type.unwrap().symbol.green(), i.name.green());},
-			ColorFormat::Yellow => {print!("{} {} ",i.file_type.unwrap().symbol.yellow(), i.name.yellow());},
-			ColorFormat::Blue => {print!("{} {} ",i.file_type.unwrap().symbol.blue(), i.name.blue());},
-			ColorFormat::Magenta => {print!("{} {} ",i.file_type.unwrap().symbol.magenta(), i.name.magenta());},
-			ColorFormat::Cyan => {print!("{} {} ",i.file_type.unwrap().symbol.cyan(), i.name.cyan());},
-			ColorFormat::White => {print!("{} {} ",i.file_type.unwrap().symbol.white(), i.name.white());},
-			ColorFormat::BrightBlack => {print!("{} {} ",i.file_type.unwrap().symbol.bright_black(), i.name.bright_black());},
-			ColorFormat::BrightRed => {print!("{} {} ",i.file_type.unwrap().symbol.bright_red(), i.name.bright_red());},
-			ColorFormat::BrightGreen => {print!("{} {} ",i.file_type.unwrap().symbol.bright_green(), i.name.bright_green());},
-			ColorFormat::BrightYellow => {print!("{} {} ",i.file_type.unwrap().symbol.bright_yellow(), i.name.bright_yellow());},
-			ColorFormat::BrightBlue => {print!("{} {} ",i.file_type.unwrap().symbol.bright_blue(), i.name.bright_blue());},
-			ColorFormat::BrightMagenta => {print!("{} {} ",i.file_type.unwrap().symbol.bright_magenta(), i.name.bright_magenta());},
-			ColorFormat::BrightCyan => {print!("{} {} ",i.file_type.unwrap().symbol.bright_cyan(), i.name.bright_cyan());},
-			ColorFormat::BrightWhite => {print!("{} {} ",i.file_type.unwrap().symbol.bright_white(), i.name.bright_white());},
-		}
+	let mut width = 0;
+	let mut height = 0;
+	let size = terminal_size();
+	if let Some((Width(w), Height(h))) = size {
+		width = w;
+		height = h;
+	} else {
+    	println!("Unable to get terminal size");
 	}
+	let mut output = Output::new(width as usize);
+	for i in filtered_files{
+		let element_text = format!("{} {}  ", i.file_type.clone().unwrap().symbol, i.name);//,i.file_type.clone().unwrap().symbol
+		// println!("{}",&element_text);
+		let element = OutputElement::new(element_text, i.file_type.unwrap().color);
+		output = output.add(element);
+	}	
+	output.print_output();
 }
---
+++ src/output_printer/formatter.rs
@@ -0,0 +1,171 @@
+use colored::Colorize;
+use crate::tomlread::ColorFormat;
+#[derive(Clone, Debug)]
+pub struct OutputElement{
+    len: usize,
+    text: String,
+	color: ColorFormat,
+}
+
+impl OutputElement{
+    pub fn new(text: String, color: ColorFormat) -> Self {
+        let mut len = 0;
+		for _i in text.chars(){
+			len += 1;
+		}
+        Self {len,text, color}
+    }
+}
+#[derive(Clone, Debug)]
+struct OutputCol {
+    element_max: usize,
+    len_vec: Vec<usize>,
+    element_vec: Vec<OutputElement>,
+}
+
+impl OutputCol{
+    fn new() -> Self{
+        let element_max = 0;
+        let len_vec = Vec::new();
+        let element_vec = Vec::new();
+        Self {element_max, len_vec, element_vec}
+    }
+    fn check_max(len_vec: &Vec<usize>)->usize{
+        let mut element_max = 0;
+        for i in len_vec{
+            if i > &element_max{
+                element_max = *i
+            }
+        }
+        element_max
+    }
+    fn add(self, output_element: OutputElement) -> Self{
+        let mut element_vec = self.element_vec;
+        let mut len_vec = self.len_vec;
+        len_vec.push(output_element.clone().len);
+        element_vec.push(output_element);
+        let element_max = Self::check_max(&len_vec);
+        Self { element_max, len_vec, element_vec}
+    }
+    fn del(self) -> (Self, OutputElement) {
+        let mut element_vec = self.element_vec;
+        let mut len_vec = self.len_vec;
+        let out_element = element_vec[0].clone();
+        element_vec.remove(0);
+        len_vec.remove(0);
+        let element_max = Self::check_max(&len_vec);
+        (Self {element_max, len_vec, element_vec}, out_element)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Output {
+    term_size: usize,
+    col_max_len_vec:Vec<usize>,
+    output_col_vec: Vec<OutputCol>,
+    row_size: usize,
+}
+
+impl Output {
+    pub fn new(term_size: usize)->Self{
+        let col_max_len_vec:Vec<usize> = Vec::new();
+        let output_col_vec: Vec<OutputCol> = Vec::new();
+        let row_size = 1;
+        Self {term_size, col_max_len_vec, output_col_vec, row_size }
+    }
+    fn is_fit(col_max_len: &Vec<usize>, term_size: usize) -> bool {
+        let mut total = 0;
+        for i in col_max_len{
+            total += i;
+        }
+        if total >= term_size{
+            return false
+        }
+        true
+    }
+    fn shift( mut output_col_vec: Vec<OutputCol>) -> (Vec<OutputCol>,Vec<usize>){
+        let mut element: OutputElement;
+        let mut col_max_len_vec: Vec<usize> = Vec::new();
+		if output_col_vec.len() != 0 {
+        	for i in 1..output_col_vec.len(){
+            	(output_col_vec[i], element) = output_col_vec[i].clone().del();
+            	output_col_vec[i-1] = output_col_vec[i-1].clone().add(element);
+        	}
+		}
+        for i in &output_col_vec{
+            col_max_len_vec.push(i.element_max);
+        }
+        if output_col_vec[output_col_vec.len()-1].element_vec.is_empty(){
+            output_col_vec.pop();
+        }
+        (output_col_vec, col_max_len_vec)
+    }
+
+    pub fn add(self, element: OutputElement)->Self{
+		// dbg!(&self);
+        let mut output_col_vec = self.output_col_vec;
+        let mut col_max_len_vec = self.col_max_len_vec;
+        let mut row_size = self.row_size;
+        if output_col_vec.len() != 0{
+            let a = output_col_vec.len()-1;
+            if output_col_vec[a].element_vec.len() <= self.row_size {
+                output_col_vec[a] = output_col_vec[a].clone().add(element);
+                col_max_len_vec[a] = output_col_vec[a].element_max;
+            } else {
+                let new_col = OutputCol::new();
+                let new_col = new_col.add(element);
+                output_col_vec.push(new_col.clone());
+                col_max_len_vec.push(new_col.element_max);
+            }
+        } else {
+            let new_col = OutputCol::new();
+            let new_col = new_col.add(element);
+            output_col_vec.push(new_col.clone());
+            col_max_len_vec.push(new_col.element_max);
+			row_size += 1;
+        }
+        while !Self::is_fit(&col_max_len_vec, self.term_size){//Error
+            (output_col_vec, col_max_len_vec) = Self::shift(output_col_vec);
+			// dbg!(&output_col_vec);
+            row_size += 1;
+        }
+        Self {term_size: self.term_size, col_max_len_vec, output_col_vec, row_size: row_size}
+    }
+    pub fn print_output(self){
+        for i in 0..self.output_col_vec[0].element_vec.len(){
+	        let mut loc: usize = 0;
+            for j in 0..self.output_col_vec.len(){
+                if i < self.output_col_vec[j].element_vec.len(){
+                        let mut space = String::new();
+                        // dbg!(loc);
+						for _k in 0..loc{
+                            space.push(' ');
+                        }
+						match &self.output_col_vec[j].element_vec[i].color{
+							ColorFormat::Black => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.black());},
+							ColorFormat::Red => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.red());},
+							ColorFormat::Green => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.green());},
+							ColorFormat::Yellow => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.yellow());},
+							ColorFormat::Blue => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.blue());},
+							ColorFormat::Magenta => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.magenta());},
+							ColorFormat::Cyan => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.cyan());},
+							ColorFormat::White => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.white());},
+							ColorFormat::BrightBlack => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_black());},
+							ColorFormat::BrightRed => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_red());},
+							ColorFormat::BrightGreen => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_green());},
+							ColorFormat::BrightYellow => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_yellow());},
+							ColorFormat::BrightBlue => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_blue());},
+							ColorFormat::BrightMagenta => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_magenta());},
+							ColorFormat::BrightCyan => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_cyan());},
+							ColorFormat::BrightWhite => {print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.bright_white());},
+						}
+                        // print!("{}{}",space, &self.output_col_vec[j].element_vec[i].text.black());
+						// dbg!(&self.output_col_vec[j].len_vec[i]);
+                        loc = self.output_col_vec[j].element_max - self.output_col_vec[j].len_vec[i];
+                }
+            }
+            println!("");
+        }
+    }
+}
+
