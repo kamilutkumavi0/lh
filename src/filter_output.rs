841a133c1d413c731fc7b4cd1d36c614c2727df0
Kamil Utku Mavi <kamilutkumavimeliora@gmail.com>
2024-12-12 09:55:14 +0300

    made function for filter

--- src/filter_output.rs
+++ src/filter_output.rs
@@ -1,60 +1,39 @@
 //! fiter_output filters and argumant with user inputs work in progress planing to filter can filter by time, file type etc.
 use crate::file_reader::Element;
 use crate::parserer::{Args, PType};
+
+
+fn file_type_filter(parsed_args: &Args, file: Element, output: &mut Vec::<Element>) {
+    match parsed_args.p_type {
+        PType::All => {
+            output.push(file);
+        }
+        PType::File => {
+            if file.is_file {
+                output.push(file);
+            }
+        }
+        PType::Dir => {
+            if file.is_dir {
+                output.push(file);
+            }
+        }
+    }
+}
+
 /// Takes parsed argumants end element vector filter files with argumant which user gives and return fitered element vector.
 pub fn filter(parsed_args: &Args, files: Vec<Element>) -> Vec<Element> {
     let mut output: Vec<Element> = Vec::new();
+
     for file in files {
         if parsed_args.all {
-            match parsed_args.p_type {
-                PType::All => {
-                    output.push(file);
-                }
-                PType::File => {
-                    if file.is_file {
-                        output.push(file);
-                    }
-                }
-                PType::Dir => {
-                    if file.is_dir {
-                        output.push(file);
-                    }
-                }
-            }
+            file_type_filter(parsed_args, file, &mut output);
         } else if parsed_args.hiden {
             if file.is_hiden {
-                match parsed_args.p_type {
-                    PType::All => {
-                        output.push(file);
-                    }
-                    PType::File => {
-                        if file.is_file {
-                            output.push(file);
-                        }
-                    }
-                    PType::Dir => {
-                        if file.is_dir {
-                            output.push(file);
-                        }
-                    }
-                }
+                file_type_filter(parsed_args, file, &mut output);
             }
         } else if !(file.is_hiden) {
-            match parsed_args.p_type {
-                PType::All => {
-                    output.push(file);
-                }
-                PType::File => {
-                    if file.is_file {
-                        output.push(file);
-                    }
-                }
-                PType::Dir => {
-                    if file.is_dir {
-                        output.push(file);
-                    }
-                }
-            }
+            file_type_filter(parsed_args, file, &mut output);
         }
     }
     output
